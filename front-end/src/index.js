e5255e72a7280ca6cbf474b1d6873206f43ada51
Sara Rekab <s.rekabtalaei@gmail.com>
2021-10-16 22:42:55 -0400

    create Helloworld App

---
+++ front-end/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css'
+
+ReactDOM.render(
+    <h1>Hello World</h1>,
+  document.getElementById('root')
+)
