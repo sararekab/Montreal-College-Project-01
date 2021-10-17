7781d7b990112a687f427c06e9de12e5d2dd24c2
Sara Rekab <s.rekabtalaei@gmail.com>
2021-10-16 22:45:53 -0400

    Create app structure for project

---
+++ front-end/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+
+
+const App = () => {
+    return (
+        <Router>
+            {/* <Header /> */}
+            <main className="py-3">
+                <Container>
+                    <Route path="/"
+                        // component={Home}
+                        exact
+                    />
+                    <Route
+                        path="/product/:id"
+                        // component={Product}
+                    />
+                    <Route
+                        path="/cart/:id?"
+                        // component={Cart}
+                    />
+                </Container>
+            </main>
+            {/* <Footer /> */}
+        </Router>
+    )
+}
+
+export default App
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,8 +5,12 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './index.css'
+import App from './App'
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+    <>
+        <App />
+        <h1>Hello World</h1>
+    </>,
   document.getElementById('root')
 )
