f16d217ba2b536612c923d0185b3a046880140a2
saraRekab <s.rekabtalaei@gmail>
2021-10-18 00:19:35 -0400

    AboutUs

--- front-end/src/pages/AboutUs.jsx
+++ front-end/src/pages/AboutUs.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
 const AboutUs = () => {
-    return ( 
-        <h1>
-            AboutUs
-        </h1>
-     );
+    return (
+        <main style={{marginTop:'40px'}}>
+                 <section>
+                    <div>
+                         <h3>What is Montreal College Shop?</h3>
+                            We created Montreal College Shop to
+                            offer you a huge assortment, more than just Best Buy products. Our Marketplace is like a virtual
+                            shopping mall that allows top-rated sellers to offer their products through MontrealCollegeShop.ca*
+                        <p>*Certain Best Buy policies including the Low Price Guarantee, Free
+                                    Shipping and Returns policies do not apply to Marketplace.</p>
+                    </div>
+
+                </section>
+                <section style={{marginTop:'40px'}}>
+                    <div>
+                        <h2>Shop With Confidence</h2>
+                        <div>
+                            <p>Carefully Selected Sellers</p>
+                            <p>Our top-rated North American sellers are handpicked by our team
+                               and follow our timely shipping and return guidelines.</p>
+                            <p>Committed to Customer Service</p>
+                            <p>We make sure our sellers offer customer service and if you ever
+                                aren’t getting the help you need – we’ll be there to step in at any point.
+                            </p>
+                            <p>Secure Payment Processing</p>
+                            <p>Best Buy handles all Marketplace transactions directly so the
+                                sellers never see your payment information.</p>
+                        </div>
+                    </div>
+                </section>
+        </main>
+    );
 }
- 
+
 export default AboutUs;
